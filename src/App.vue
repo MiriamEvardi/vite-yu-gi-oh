84415fe81a4b496d8b2dd24ea2c320122baba945
MiriamEvardi <miriamevardi97@gmail.com>
2024-03-04 15:13:26 +0100

    setting API

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,28 @@
 <script>
+import axios from 'axios';
+import { store } from './data/store';
+
+export default {
+  data() {
+    return {
+      cards: [],
+
+      store,
+    }
+  },
+
+  created() {
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(res => {
+      this.store.cards = res.data.results;
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+
+  components: {
+
+  }
+}
 
 </script>
 
